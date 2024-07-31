28ad52caee7cc834b6d95874929190c224ed7aef
Carl Ollington <Carl.Ollington@outlook.com>
2024-07-31 10:06:09 +0100

    corrected normalize css implemention

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
     <title>Where in the World?</title>
     <meta charset="UTF-8" name="viewport" content="width=device-width,initial-scale=1" />
-    <link href="witw_styles.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="witw_styles.css" rel="stylesheet">
 </head>
 <body class="page">
 <header class="header">
